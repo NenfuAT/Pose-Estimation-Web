61e6d849226275d174c5414878f528b3c49507ad
k21091 <k21091kk@aitech.ac.jp>
2024-06-01 18:40:17 +0900

    環境変数のテスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+interface HomeProps {
+  domain: string;
+}
+
+export default function Home({domain}: HomeProps) {
   return (
     <main className={styles.main}>
       <div>
         <a>　　　　　　　　　　　　　　　　　　　　　　　　Next.js完全に理解した</a>
       </div>
+      <div>
+        <h1>ドメイン:</h1>
+        <p>{domain}</p>
+      </div>
     </main>
   );
+}
+
+export const getServerSideProps = async() => {
+  const domain = process.env.DOMAIN_NAME;
+  return{
+    props: {
+      domain,
+    }
+  }
 }
