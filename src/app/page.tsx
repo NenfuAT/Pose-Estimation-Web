89d3910258928507b3a3b6dc5ec7c47117aa8a81
k21091 <k21091kk@aitech.ac.jp>
2024-06-01 18:25:37 +0900

    fix:不要部分の削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,3 @@ export default function Home() {
     </main>
   );
 }
-export const getServerSideProps = async() => {
-  const domain = process.env.DOMAIN_NAME;
-  return{
-    props: {
-      domain,
-    }
-  }
-}
