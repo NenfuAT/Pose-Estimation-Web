1f794a9f37fd0b5f47047ab8801c9384d22cf079
k21091 <k21091kk@aitech.ac.jp>
2024-06-01 19:30:38 +0900

    fix:fechに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`${process.env.DOMAIN_NAME}/api/bucket/list`);
+        const result = await res.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   
   return (
     <main className={styles.main}>
